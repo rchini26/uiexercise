abd743a8222a6684bb9c39118f1f1c8e9959697f
rchini26 <rafa.montm@gmail.com>
2026-01-06 21:58:26 -0300

    Added Dark Theme UI

---
+++ Assets/Scripts/Screen/ScreenBase.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+namespace Screens
+{
+    public enum ScreenType
+    {
+        Shop_Panel,
+        
+        
+    }
+    public class ScreenBase : MonoBehaviour
+    {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
