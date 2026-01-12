7f4398cc63778f19adcf5cce0ef1e8e9226e8add
rchini26 <rafa.montm@gmail.com>
2026-01-12 11:21:00 -0300

    Added Extension

--- Assets/Scripts/Core/MagicUtil.cs
+++ Assets/Scripts/Core/MagicUtil.cs
@@ -1,16 +1,43 @@
 using UnityEngine;
+using DG.Tweening;
+using Random = UnityEngine.Random;
+using System.Collections.Generic;
 
-public class MagicUtil : MonoBehaviour
+public static class MagicUtil
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public static SphereCollider AddTrigger(Transform parent, float radius = 1)
     {
+        GameObject _trigger = new GameObject("Trigger", typeof(SphereCollider));
+        _trigger.transform.SetParent(parent);
+        _trigger.transform.localPosition = Vector3.zero;
+        _trigger.transform.localScale = Vector3.one;
+        _trigger.layer = LayerMask.NameToLayer("Trigger");
         
+        SphereCollider _triggerCollider = _trigger.GetComponent<SphereCollider>();
+        _triggerCollider.radius = radius;
+        _triggerCollider.isTrigger = true;
+        
+        return _triggerCollider;
     }
 
-    // Update is called once per frame
-    void Update()
+    #region SCALES
+    public static void Scale(this Transform t, float size = 1.2f)
     {
-        
+        t.localScale = Vector3.one * size;
     }
+    #endregion
+
+    #region RANDOMIZE
+    public static T GetRandom<T>(this List<T> list)
+    {
+        return list[Random.Range(0, list.Count)];
+    }
+
+    public static T GetRandom<T>(this T[] array)
+    {
+        if (array.Length == 0) return default(T);
+        
+        return array[Random.Range(0, array.Length)];
+    }
+    #endregion
 }
